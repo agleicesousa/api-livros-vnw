5521fe43adc78f95fd0350ca50cb590f15852624
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-29 03:30:37 -0300

    Alterando para rodar no host público

--- app.py
+++ app.py
@@ -215,4 +215,4 @@ def deletar_livro(id):
         return jsonify({"erro": f"Erro ao deletar livro no banco de dados: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
